bfd9065aade9ee82778172ee1a0e4a85d95a99d5
William Lee <williamlee021@gmail.com>
2018-01-22 09:54:20 -0500

    removed background

--- Components/Home.js
+++ Components/Home.js
@@ -3,15 +3,15 @@ import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 const Home = ({ navigation }) => (
-  <ImageBackground source={require('../Assets/background.jpg')} style={styles.container}>
-    <Text>WHAT IS THE PURPOSE OF THIS APP?</Text>
+  <View>
+    <Text style={styles.container}>AVALON</Text>
     <Text>You will be asked a couple questions to help set up your game</Text>
     <Text>Once when you have that set up, this app will narrate the game for you</Text>
     <Button
       onPress={() => navigation.navigate('Setup')}
       title="Setup"
     />
-  </ImageBackground>
+  </View>
 );
 
 const styles = StyleSheet.create({
