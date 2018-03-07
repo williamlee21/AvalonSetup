ef55118844571069a65f76150a2c373ef3ea62f9
William Lee <williamlee021@gmail.com>
2018-03-07 11:57:05 -0500

    starting point

--- Components/Narration.js
+++ Components/Narration.js
@@ -13,7 +13,7 @@ export default class Narration extends Component {
                 {/* Oberon */}
                 {!character.Oberon && Expo.Speech.speak('Minions of Mordred, open your eyes and look around so you know the agents of evil.', {rate:.9})}
                 {character.Oberon && Expo.Speech.speak('Minions of Mordred, except Oberon, open your eyes and look around so you know the agents of evil.', {rate:.9})}
-                {character.Oberon && Expo.Speech.speak('5, 4, 3, 2, 1', {rate:0.2})}
+                {Expo.Speech.speak('5, 4, 3, 2, 1', {rate:0.2})}
                 {Expo.Speech.speak('Minions of Mordred, close your eyes.', {rate:.9})}
                 {/* Merlin and Mordred */}
                 {character.Merlin && !character.Mordred && Expo.Speech.speak(' Minions of Mordred, extend your thumb so Merlin will know of you.', {rate:.9})}
@@ -24,7 +24,7 @@ export default class Narration extends Component {
                 {character.Percival && Expo.Speech.speak('Merlin and Morgana, extend your thumbs so Percival will know of you. Percival, open your eyes so that you may know Merlin and Morgana.', {rate:.9})}
                 {character.Percival && Expo.Speech.speak('5, 4, 3, 2, 1', {rate:0.2})}
                 {character.Percival && Expo.Speech.speak('Merlin and Morgana, put your thumbs down. Percival, close your eyes.', {rate:.9})}
-                {Expo.Speech.speak('All players should have their eyes closed and hands in a fist in front of them. Everyone, open your eyes', {rate:.9})}
+                {Expo.Speech.speak('All players should have their eyes closed and hands in a fist in front of them. Everyone, open your eyes', {rate:1})}
 
                 <Button onPress={() => Expo.Speech.stop()} title='Stop reading' /> 
             </View>
